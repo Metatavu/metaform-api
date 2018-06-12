4e5eaedf72affd796433c19044eccd0fc78ae14b
Antti Leppä <antti.leppa@metatavu.fi>
2018-06-12 12:36:57 +0300

    Added more tests

--- src/test/java/fi/metatavu/metaform/server/MetaformTestsIT.java
+++ src/test/java/fi/metatavu/metaform/server/MetaformTestsIT.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -29,6 +30,36 @@ public class MetaformTestsIT extends AbstractIntegrationTest {
       assertEquals("text", metaform.getSections().get(0).getFields().get(0).getName());
       assertEquals("text", metaform.getSections().get(0).getFields().get(0).getType().toString());
       assertEquals("Text field", metaform.getSections().get(0).getFields().get(0).getTitle());
+    } finally {
+      dataBuilder.clean();
+    }
+  }
+  
+  @Test
+  public void testFindMetaform() throws IOException, URISyntaxException {
+    TestDataBuilder dataBuilder = new TestDataBuilder(this, REALM_1, "test1.realm1", "test");
+    try {
+      MetaformsApi adminMetaformsApi = dataBuilder.getAdminMetaformsApi();
+      Metaform metaform = dataBuilder.createMetaform("simple");
+      Metaform foundMetaform = adminMetaformsApi.findMetaform(REALM_1, metaform.getId());
+      assertEquals(metaform.toString(), foundMetaform.toString());
+    } finally {
+      dataBuilder.clean();
+    }
+  }
+
+  @Test
+  public void testListMetaform() throws IOException, URISyntaxException {
+    TestDataBuilder dataBuilder = new TestDataBuilder(this, REALM_1, "test1.realm1", "test");
+    try {
+      MetaformsApi adminMetaformsApi = dataBuilder.getAdminMetaformsApi();
+      Metaform metaform1 = dataBuilder.createMetaform("simple");
+      Metaform metaform2 = dataBuilder.createMetaform("simple");
+
+      List<Metaform> list = adminMetaformsApi.listMetaforms(REALM_1);
+
+      assertEquals(metaform1.toString(), list.get(0).toString());
+      assertEquals(metaform2.toString(), list.get(1).toString());
     } finally {
       dataBuilder.clean();
     }
