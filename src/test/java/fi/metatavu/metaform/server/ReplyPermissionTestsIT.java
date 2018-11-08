9c7e2302cad0bf950e6109948e892a17999dbfe8
Antti Leppä <antti.leppa@metatavu.fi>
2018-11-08 18:51:53 +0200

    Added support for UMA tickets in integration tests

---
+++ src/test/java/fi/metatavu/metaform/server/ReplyPermissionTestsIT.java
@@ -0,0 +1,213 @@
+package fi.metatavu.metaform.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import feign.FeignException;
+import fi.metatavu.metaform.client.Metaform;
+import fi.metatavu.metaform.client.MetaformsApi;
+import fi.metatavu.metaform.client.RepliesApi;
+import fi.metatavu.metaform.client.Reply;
+import fi.metatavu.metaform.client.ReplyData;
+import fi.metatavu.metaform.server.rest.ReplyMode;
+
+@SuppressWarnings ("squid:S1192")
+public class ReplyPermissionTestsIT extends AbstractIntegrationTest {
+  
+  /**
+  3) Testi, joka tarkistaa, että metaform super saa vastauksen
+  4) Testi, joka tarkistaa, että metaform admin saa vastauksen
+  
+  6) Testi, joka tarkistaa, että reply:notify saa sähköpostin uudesta
+  7) Testi, joka taristaa, että reply:notify saa sähköpostin muutoksesta mutta edellinen ei saa
+  8) Testi, joka tarkistaa, ettei muutos ilman reply:notify muutosta lähetä sähköpostia
+  9) Realm admin näkee keiken
+  **/
+  
+  /**
+   * Test that asserts that user may find his / her own reply
+   */
+  @Test
+  public void findOwnReply() throws IOException, URISyntaxException {
+    String adminToken = getAdminToken(REALM_1);
+    String accessToken = getAccessToken(REALM_1, "test1.realm1", "test");
+    MetaformsApi adminMetaformsApi = getMetaformsApi(adminToken);
+    RepliesApi repliesApi = getRepliesApi(accessToken);
+    RepliesApi adminRepliesApi = getRepliesApi(adminToken);
+    
+    Metaform metaform = adminMetaformsApi.createMetaform(REALM_1, readMetaform("simple-permission-context"));
+    try {
+      Reply createdReply = repliesApi.createReply(REALM_1, metaform.getId(), createPermisionSelectReply("group-1"), null, ReplyMode.REVISION.toString());
+      try {
+        Reply foundReply = repliesApi.findReply(REALM_1, metaform.getId(), createdReply.getId());
+        assertNotNull(foundReply);
+      } finally {
+        adminRepliesApi.deleteReply(REALM_1, metaform.getId(), createdReply.getId());
+      }
+    } finally {
+      adminMetaformsApi.deleteMetaform(REALM_1, metaform.getId());
+    }
+  }
+  
+  /**
+   * Test that asserts that anonymous users may not find their "own" replies
+   */
+  @Test
+  public void findOwnReplyAnonymous() throws IOException, URISyntaxException {
+    String adminToken = getAdminToken(REALM_1);
+    String anonymousToken = getAnonymousToken(REALM_1);
+    
+    MetaformsApi adminMetaformsApi = getMetaformsApi(adminToken);
+    RepliesApi anonRepliesApi = getRepliesApi(anonymousToken);
+    RepliesApi adminRepliesApi = getRepliesApi(adminToken);
+    
+    Metaform metaform = adminMetaformsApi.createMetaform(REALM_1, readMetaform("simple-permission-context-anon"));
+    try {
+      Reply createdReply = anonRepliesApi.createReply(REALM_1, metaform.getId(), createPermisionSelectReply("group-1"), null, ReplyMode.REVISION.toString());
+      try {
+        assertForbiddenToFindReply(anonymousToken, REALM_1, metaform.getId(), createdReply.getId());
+      } finally {
+        adminRepliesApi.deleteReply(REALM_1, metaform.getId(), createdReply.getId());
+      }
+    } finally {
+      adminMetaformsApi.deleteMetaform(REALM_1, metaform.getId());
+    }
+  }
+  
+  /**
+   * Test that asserts that other users may not find their replies
+   */
+  @Test
+  public void findOthersReplyUser() throws IOException, URISyntaxException {
+    String adminToken = getAdminToken(REALM_1);
+    String accessToken1 = getAccessToken(REALM_1, "test1.realm1", "test");
+    String accessToken2 = getAccessToken(REALM_1, "test2.realm1", "test");
+
+    MetaformsApi adminMetaformsApi = getMetaformsApi(adminToken);
+    RepliesApi adminRepliesApi = getRepliesApi(adminToken);
+
+    RepliesApi repliesApi = getRepliesApi(accessToken1);
+
+    Metaform metaform = adminMetaformsApi.createMetaform(REALM_1, readMetaform("simple-permission-context"));
+    try {
+      Reply createdReply = repliesApi.createReply(REALM_1, metaform.getId(), createPermisionSelectReply("group-1"), null, ReplyMode.REVISION.toString());
+      try {
+        assertForbiddenToFindReply(accessToken2, REALM_1, metaform.getId(), createdReply.getId());
+      } finally {
+        adminRepliesApi.deleteReply(REALM_1, metaform.getId(), createdReply.getId());
+      }
+    } finally {
+      adminMetaformsApi.deleteMetaform(REALM_1, metaform.getId());
+    }
+  }
+
+  /**
+   * Test that asserts that metaform-admin may find replies created by others
+   */
+  @Test
+  public void findOthersReplyAdmin() throws IOException, URISyntaxException {
+    String adminToken = getAdminToken(REALM_1);
+    String accessToken = getAccessToken(REALM_1, "test1.realm1", "test");
+    MetaformsApi adminMetaformsApi = getMetaformsApi(adminToken);
+    RepliesApi repliesApi = getRepliesApi(accessToken);
+    RepliesApi adminRepliesApi = getRepliesApi(adminToken);
+    
+    Metaform metaform = adminMetaformsApi.createMetaform(REALM_1, readMetaform("simple-permission-context"));
+    try {
+      Reply createdReply = repliesApi.createReply(REALM_1, metaform.getId(), createPermisionSelectReply("group-1"), null, ReplyMode.REVISION.toString());
+      try {
+        Reply foundReply = adminRepliesApi.findReply(REALM_1, metaform.getId(), createdReply.getId());
+        assertNotNull(foundReply);
+      } finally {
+        adminRepliesApi.deleteReply(REALM_1, metaform.getId(), createdReply.getId());
+      }
+    } finally {
+      adminMetaformsApi.deleteMetaform(REALM_1, metaform.getId());
+    }
+  }
+  
+
+  
+  // Test that asserts that user may list only his / her own replies
+  // Test that asserts that user in permission context group may see replies targeted to that group
+  // Test that asserts that admin may list all replies
+  
+  /**
+   * Test that asserts that user may list only his / her own replies
+   */
+  @Test
+  public void listOwnReplies() throws IOException, URISyntaxException {
+    String adminToken = getAdminToken(REALM_1);
+    
+    String accessToken1 = getAccessToken(REALM_1, "test1.realm1", "test");
+    String accessToken2 = getAccessToken(REALM_1, "test2.realm1", "test");
+    String accessToken3 = getAccessToken(REALM_1, "test3.realm1", "test");
+    
+    MetaformsApi adminMetaformsApi = getMetaformsApi(adminToken);
+    RepliesApi adminRepliesApi = getRepliesApi(adminToken);
+    RepliesApi repliesApi1 = getRepliesApi(accessToken1);
+    RepliesApi repliesApi2 = getRepliesApi(accessToken2);
+    RepliesApi repliesApi3 = getRepliesApi(accessToken3);
+    
+    Metaform metaform = adminMetaformsApi.createMetaform(REALM_1, readMetaform("simple-permission-context"));
+    try {
+      Reply reply1 = repliesApi1.createReply(REALM_1, metaform.getId(), createPermisionSelectReply("group-1"), null, ReplyMode.REVISION.toString());
+      Reply reply2 = repliesApi2.createReply(REALM_1, metaform.getId(), createPermisionSelectReply("group-2"), null, ReplyMode.REVISION.toString());
+      Reply reply3 = repliesApi3.createReply(REALM_1, metaform.getId(), createPermisionSelectReply("group-3"), null, ReplyMode.REVISION.toString());
+      
+      try {
+        List<Reply> replies = repliesApi1.listReplies(REALM_1, metaform.getId(), Collections.emptyMap());
+        assertEquals(replies.size(), 1);
+        assertEquals(replies.get(0).getId(), reply1.getId());
+      } finally {
+        adminRepliesApi.deleteReply(REALM_1, metaform.getId(), reply1.getId());
+        adminRepliesApi.deleteReply(REALM_1, metaform.getId(), reply2.getId());
+        adminRepliesApi.deleteReply(REALM_1, metaform.getId(), reply3.getId());
+      }
+      
+    } finally {
+      adminMetaformsApi.deleteMetaform(REALM_1, metaform.getId());
+    }
+  }
+  
+  /**
+   * Creates permission select reply with given value
+   * 
+   * @param value value
+   * @return permission select reply with given value
+   */
+  private Reply createPermisionSelectReply(String value) {
+    ReplyData replyData = new ReplyData();
+    replyData.put("permission-select", value);
+    Reply reply = createReplyWithData(replyData);
+    return reply;
+  }
+  
+  /**
+   * Asserts that reply is forbidden to find
+   * 
+   * @param token token
+   * @param realmId realm
+   * @param metaformId metaform
+   * @param replyId replay
+   */
+  private void assertForbiddenToFindReply(String token, String realmId, UUID metaformId, UUID replyId) {
+    RepliesApi repliesApi = getRepliesApi(token);
+    try {
+      repliesApi.findReply(realmId, metaformId, replyId);
+      fail(String.format("Reply %s should not be accessible", replyId.toString()));
+    } catch (FeignException e) {
+      assertEquals(403, e.status());
+    }
+  }
+  
+}
---
+++ src/test/java/fi/metatavu/metaform/server/UmaErrorDecoder.java
@@ -0,0 +1,133 @@
+package fi.metatavu.metaform.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import feign.Response;
+import feign.RetryableException;
+import feign.codec.ErrorDecoder.Default;
+import fi.metatavu.metaform.ApiClient;
+
+/**
+ * Feign error decoder that handles UMA token switching
+ * 
+ * @author Antti Leppä
+ */
+public class UmaErrorDecoder extends Default {
+  
+  private String authorization;
+  private ApiClient apiClient;
+  
+  /**
+   * Constructor
+   * 
+   * @param authorization original authorization string
+   * @param apiClient api client
+   */
+  public UmaErrorDecoder(String authorization, ApiClient apiClient) {
+    super();
+    this.authorization = authorization;
+    this.apiClient = apiClient;
+  }
+
+  @Override
+  public Exception decode(String methodKey, Response response) {
+    if (response.status() == 401) {      
+      Map<String, String> umaTicket = getUmaTicket(response);
+      if (umaTicket != null) {
+        try {
+          String rpt = getRPT(authorization, umaTicket);
+          if (rpt != null) {
+            apiClient.setApiKey(String.format("Bearer %s", rpt));
+            return new RetryableException("UMA", null);
+          } else {
+            return new RptForbiddenFeignException("No RPT Token");
+          }
+          
+        } catch (UnsupportedOperationException | IOException e) {
+          return e;
+        }
+      } else {
+        return new RptForbiddenFeignException("No UMA Ticket");
+      }
+    }
+    
+    return super.decode(methodKey, response);
+  }
+  
+  /**
+   * Returns UMA ticket from www-authenticate header or null if not found
+   * 
+   * @param response response
+   * @returns {Object} UMA ticket components
+   */
+  private Map<String, String> getUmaTicket(Response response) {
+    Collection<String> headerValues = response.headers().get("www-authenticate");
+    if (!headerValues.isEmpty()) {
+      String authenticate = headerValues.iterator().next();
+      if (StringUtils.startsWith(authenticate, "UMA ")) {
+        return Arrays.stream(StringUtils.split(StringUtils.stripStart(authenticate, "UMA "), ','))
+          .collect(Collectors.toMap((component) -> {
+            return StringUtils.strip(StringUtils.substringBefore(component, "="), "\"");
+          }, (component) -> {
+            return StringUtils.strip(StringUtils.substringAfter(component, "="), "\"");
+          }));
+      }
+    }
+    
+    return null;
+  }
+  
+  /**
+   * Retrieves RPT token
+   * 
+   * @param authorization authorization
+   * @param ticket UMA ticket
+   * @return token token or null if token creation has fails
+   * @throws IOException when io error fails
+   */
+  private String getRPT(String authorization, Map<String, String> ticket) throws IOException {
+    String url = String.format("%s/protocol/openid-connect/token", ticket.get("as_uri"));
+    
+    try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+      HttpPost post = new HttpPost(url);
+      EntityBuilder entityBuilder = EntityBuilder.create();
+      
+      entityBuilder.setParameters(
+        new BasicNameValuePair("grant_type", "urn:ietf:params:oauth:grant-type:uma-ticket"),
+        new BasicNameValuePair("ticket", ticket.get("ticket")),
+        new BasicNameValuePair("submit_request", "false")
+      );
+      
+      post.setHeader("Authorization", authorization);
+      post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+      
+      post.setEntity(entityBuilder.build());
+      HttpResponse response = client.execute(post);
+      HttpEntity httpEntity = response.getEntity();
+
+      try (InputStream body = httpEntity.getContent()) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> token = objectMapper.readValue(body, new TypeReference<Map<String, Object>>() { });
+        return (String) token.get("access_token");
+      }
+    }
+  }
+  
+}
