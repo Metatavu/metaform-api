8476aa1cfb4cb220189a7dc70f0f65b82a01512f
Antti Leppä <antti.leppa@metatavu.fi>
2018-11-08 18:38:47 +0200

    Changed Keycloak config resolver to inject policy enforser config

--- src/main/java/fi/metatavu/metaform/server/keycloak/MetaformKeycloakConfigResolver.java
+++ src/main/java/fi/metatavu/metaform/server/keycloak/MetaformKeycloakConfigResolver.java
@@ -3,6 +3,8 @@ package fi.metatavu.metaform.server.keycloak;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +13,11 @@ import org.keycloak.adapters.KeycloakDeployment;
 import org.keycloak.adapters.KeycloakDeploymentBuilder;
 import org.keycloak.adapters.spi.HttpFacade.Request;
 import org.keycloak.representations.adapters.config.AdapterConfig;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig.EnforcementMode;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig.MethodConfig;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig.PathConfig;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig.UserManagedAccessConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,7 +54,11 @@ public class MetaformKeycloakConfigResolver implements KeycloakConfigResolver {
       try {
         configStream = new FileInputStream(configFile);
         try {
-          return KeycloakDeploymentBuilder.build(configStream);
+          AdapterConfig adapterConfig = KeycloakDeploymentBuilder.loadAdapterConfig(configStream);          
+          adapterConfig.setPolicyEnforcerConfig(createPolicyEnforcerConfig());
+          
+          KeycloakDeployment deployment = KeycloakDeploymentBuilder.build(adapterConfig);
+          return deployment;
         } finally {
           configStream.close();
         }
@@ -64,6 +75,74 @@ public class MetaformKeycloakConfigResolver implements KeycloakConfigResolver {
     adapterConfig.setAuthServerUrl("http://localhost:123");
     
     return KeycloakDeploymentBuilder.build(adapterConfig);
+  }
+
+  /**
+   * Creates policy enforcer config
+   * 
+   * @return policy enforcer config
+   */
+  private PolicyEnforcerConfig createPolicyEnforcerConfig() {
+    PolicyEnforcerConfig result = new PolicyEnforcerConfig();
+    result.setEnforcementMode(EnforcementMode.PERMISSIVE);
+    result.setUserManagedAccess(new UserManagedAccessConfig());
+    result.setPaths(Arrays.asList(createRepliesPolicyEnforcerConfigPath(), createReplyPolicyEnforcerConfigPath()));
+    return result;
+  }
+  
+  /**
+   * Creates replies policy enforcer path config
+   * 
+   * @return replies policy enforcer path config
+   */
+  private PathConfig createRepliesPolicyEnforcerConfigPath() {
+    return createPolicyEnforcerConfigPath("Replies", "/v1/realms/{realmId}/metaforms/{metaformId}/replies", Arrays.asList(
+      createPolicyEnforcerMethodConfig("GET", Arrays.asList("reply:view")),
+      createPolicyEnforcerMethodConfig("POST", Arrays.asList("reply:create"))
+    ));
+  }
+  
+  /**
+   * Creates reply policy enforcer path config
+   * 
+   * @return reply policy enforcer path config
+   */
+  private PathConfig createReplyPolicyEnforcerConfigPath() {
+    return createPolicyEnforcerConfigPath("Reply", "/v1/realms/{realmId}/metaforms/{metaformId}/replies/{id}", Arrays.asList(
+      createPolicyEnforcerMethodConfig("GET", Arrays.asList("reply:view")),
+      createPolicyEnforcerMethodConfig("PUT", Arrays.asList("reply:edit")),
+      createPolicyEnforcerMethodConfig("DELETE", Arrays.asList("reply:edit"))
+    ));
+  }
+  
+  /**
+   * Creates policy enforcer method config
+   * 
+   * @param method method
+   * @param scopes scopes
+   * @return policy enforcer method config
+   */
+  private MethodConfig createPolicyEnforcerMethodConfig(String method, List<String> scopes) {
+    MethodConfig result = new MethodConfig();
+    result.setMethod(method);
+    result.setScopes(scopes);
+    return result;
+  }
+  
+  /**
+   * Creates policy enforcer config path
+   * 
+   * @param name name
+   * @param path path
+   * @param methods methods
+   * @return create path config
+   */
+  private PathConfig createPolicyEnforcerConfigPath(String name, String path, List<MethodConfig> methods) {
+    PathConfig result = new PathConfig();
+    result.setPath(path);
+    result.setName(name);
+    result.setMethods(methods);
+    return result;
   }
 
 }
