8bcc2b7beef6372aa0fc7fe2c5888469533b2438
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-02 12:11:19 +0300

    Sonar

--- travis.sh
+++ travis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean verify coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -DrepoToken=$COVERALLS_TOKEN --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+mvn clean verify org.jacoco:jacoco-maven-plugin:prepare-agent coveralls:report sonar:sonar -DrepoToken=$COVERALLS_TOKEN --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
