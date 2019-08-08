aad318d5d2d290a020cb15f7c93de68dd6b4e32e
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-08 12:59:29 +0300

    MySQL

--- scripts/setup-db-mysql.sql
+++ scripts/setup-db-mysql.sql
@@ -1,4 +1,4 @@
-create user sa IDENTIFIED WITH mysql_native_password BY 'sa';
+create user sa IDENTIFIED BY 'sa';
 grant all on mftest.* to sa; 
 drop database if exists mftest; 
 CREATE DATABASE `mftest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */
