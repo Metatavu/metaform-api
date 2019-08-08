e305ff37a4869d8dac63b1222fcba21b92ccfc13
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-08 12:50:23 +0300

    Travis MySQL

--- scripts/setup-db-mysql.sql
+++ scripts/setup-db-mysql.sql
@@ -1,5 +1,5 @@
 create user if not exists sa identified by 'sa';
 ALTER USER sa IDENTIFIED WITH mysql_native_password BY 'sa';
 grant all on .* to sa; 
-drop database if exists ; 
-create database default charset utf8;
+drop database if exists mftest; 
+CREATE DATABASE `mftest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */
