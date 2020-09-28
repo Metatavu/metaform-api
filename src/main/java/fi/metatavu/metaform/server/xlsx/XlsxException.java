7c10e06eb4852b035f747b964de19f5a07f7ef92
Antti Leppä <antti.leppa@metatavu.fi>
2020-09-28 08:10:20 +0300

    Review changes

--- src/main/java/fi/metatavu/metaform/server/xlsx/XlsxException.java
+++ src/main/java/fi/metatavu/metaform/server/xlsx/XlsxException.java
@@ -8,10 +8,21 @@ public class XlsxException extends Exception {
 
   private static final long serialVersionUID = 8611634313163534478L;
 
+  /**
+   * Constructor
+   * 
+   * @param message message
+   * @param original original exception
+   */
   public XlsxException(String message, Throwable original) {
     super(message, original);
   }
   
+  /**
+   * Constructor
+   * 
+   * @param message message
+   */
   public XlsxException(String message) {
     super(message);
   }
