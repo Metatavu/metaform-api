3c5e5785a978cd272da5d137f72685e25ade6925
Antti Leppä <antti.leppa@metatavu.fi>
2020-09-28 08:09:03 +0300

    Update src/main/java/fi/metatavu/metaform/server/xlsx/AbstractXlsxBuilder.java
    Co-authored-by: Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>

--- src/main/java/fi/metatavu/metaform/server/xlsx/AbstractXlsxBuilder.java
+++ src/main/java/fi/metatavu/metaform/server/xlsx/AbstractXlsxBuilder.java
@@ -151,7 +151,7 @@ public abstract class AbstractXlsxBuilder<B extends org.apache.poi.ss.usermodel.
    * @param sheetId sheet id
    * @param rowNumber row number
    * @param columnNumber column number
-   * @param value value
+   * @param value value as integer
    * @return cell
    */
   public Cell setCellValue(String sheetId, int rowNumber, int columnNumber, Integer value) {
