736e7d3cbaa630d24e8fcd8876931e94c08619d7
Tianxing Wu <chi0chu319@gmail.com>
2022-07-27 22:43:45 +0300

    added buffer reader

--- src/main/kotlin/fi/metatavu/metaform/server/rest/WildcardRequestBodyReader.kt
+++ src/main/kotlin/fi/metatavu/metaform/server/rest/WildcardRequestBodyReader.kt
@@ -1,8 +1,10 @@
 package fi.metatavu.metaform.server.rest
 
 import org.slf4j.Logger
+import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
+import java.io.InputStreamReader
 import java.lang.reflect.Type
 import javax.inject.Inject
 import javax.ws.rs.Consumes
@@ -11,11 +13,10 @@ import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.MultivaluedMap
 import javax.ws.rs.ext.MessageBodyReader
 import javax.ws.rs.ext.Provider
-import javax.xml.bind.JAXB
 
 @Provider
 @Consumes("*/*")
-class MyRequestBodyReader : MessageBodyReader<String> {
+class MyRequestBodyReader : MessageBodyReader<String?> {
 
     @Inject
     lateinit var logger: Logger
@@ -31,14 +32,20 @@ class MyRequestBodyReader : MessageBodyReader<String> {
 
     @Throws(IOException::class, WebApplicationException::class)
     override fun readFrom(
-        type: Class<String>,
+        type: Class<String?>,
         genericType: Type,
         annotations: Array<Annotation>,
-        mediaType: MediaType?,
+        mediaType: MediaType,
         httpHeaders: MultivaluedMap<String, String>,
-        entityStream: InputStream?
+        entityStream: InputStream
     ): String {
-        logger.info(String.format("JAXB.unmarshal(entityStream, String::class.java): %s", JAXB.unmarshal(entityStream, String::class.java)))
-        return JAXB.unmarshal(entityStream, String::class.java)
+        logger.info("readFrom")
+        logger.info(String.format("entityStream.toString(): %s", entityStream.toString()))
+//        logger.info(String.format("JAXB.unmarshal(entityStream, String::class.java): %s", JAXB.unmarshal(entityStream, String::class.java)))
+
+        BufferedReader(InputStreamReader(entityStream)).use { br ->
+            return br.readLine()
+        }
+
     }
 }
