5df240676acad32e35b8b2450505173a6f0e10af
Antti Leppä <antti.leppa@metatavu.fi>
2022-08-16 17:08:48 +0300

    Added docs to MetaformUtils

--- src/main/kotlin/fi/metatavu/metaform/server/utils/MetaformUtils.kt
+++ src/main/kotlin/fi/metatavu/metaform/server/utils/MetaformUtils.kt
@@ -1,12 +1,20 @@
 package fi.metatavu.metaform.server.utils
 
-import com.github.slugify.Slugify
 import fi.metatavu.metaform.api.spec.model.Metaform
 import fi.metatavu.metaform.api.spec.model.MetaformField
 import fi.metatavu.metaform.api.spec.model.MetaformSection
-import org.apache.commons.lang3.StringUtils
 
+/**
+ * Metaform utilities
+ */
 object MetaformUtils {
+
+    /**
+     * Returns flatted list of metaform fields
+     *
+     * @param metaform metaform
+     * @return list of metaform fields
+     */
     fun getMetaformFields(metaform: Metaform): List<MetaformField> {
         return metaform.sections
                 ?.mapNotNull(MetaformSection::fields)
