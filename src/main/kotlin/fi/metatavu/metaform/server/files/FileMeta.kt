6f042975652c8f5927738e2e94b4e14299872e2e
Antti Leppä <antti.leppa@metatavu.fi>
2023-02-12 22:07:06 +0200

    Native

--- src/main/kotlin/fi/metatavu/metaform/server/files/FileMeta.kt
+++ src/main/kotlin/fi/metatavu/metaform/server/files/FileMeta.kt
@@ -1,7 +1,9 @@
 package fi.metatavu.metaform.server.files
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.quarkus.runtime.annotations.RegisterForReflection
 
+@RegisterForReflection
 data class FileMeta (
         @JsonProperty("contentType")
         val contentType: String,
