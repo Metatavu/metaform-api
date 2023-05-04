93aa587868c510b9edaaf740680005dcc7c9b099
Simeon <simeon.platonov@gmail.com>
2023-05-04 14:39:42 +0300

    Changing tests to use system wide scripts

--- src/main/kotlin/fi/metatavu/metaform/server/rest/translate/ScriptsTranslator.kt
+++ src/main/kotlin/fi/metatavu/metaform/server/rest/translate/ScriptsTranslator.kt
@@ -1,14 +1,12 @@
 package fi.metatavu.metaform.server.rest.translate
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import fi.metatavu.metaform.server.exceptions.MalformedMetaformJsonException
 import fi.metatavu.metaform.server.persistence.model.Script
-import java.io.IOException
+import javax.enterprise.context.ApplicationScoped
 
 /**
  * Translator for scripts
  */
+@ApplicationScoped
 class ScriptsTranslator {
   fun translate(entity: Script): fi.metatavu.metaform.api.spec.model.Script {
     return fi.metatavu.metaform.api.spec.model.Script(
