d69414a949afa52595454025ded23636c5a09ebe
Simeon Platonov <41298320+platonovsimeon@users.noreply.github.com>
2023-05-08 14:57:12 +0300

    Update MigrateDeprecatedFieldProperties.kt

--- src/main/kotlin/fi/metatavu/metaform/server/liquibase/changes/MigrateDeprecatedFieldProperties.kt
+++ src/main/kotlin/fi/metatavu/metaform/server/liquibase/changes/MigrateDeprecatedFieldProperties.kt
@@ -1,12 +1,14 @@
 package fi.metatavu.metaform.server.liquibase.changes
 
-import fi.metatavu.metaform.api.spec.model.MetaformFieldType
-import fi.metatavu.metaform.api.spec.model.MetaformSection
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.quarkus.runtime.annotations.RegisterForReflection
 import liquibase.database.Database
 import liquibase.database.jvm.JdbcConnection
 import liquibase.exception.CustomChangeException
 import java.util.*
+
 
 /**
  * Custom change for migrating deprecated field properties
@@ -26,20 +28,52 @@ class MigrateDeprecatedFieldProperties : AbstractAuthzCustomChange() {
             val metaformId = it.first
             val metaform = it.second
 
-            appendConfirmationMessage("Migrating deprecated field properties for metaform ${metaform.slug}")
+            appendConfirmationMessage("Migrating deprecated field properties for metaform ${metaform["slug"]}")
 
-            val tableFields = metaform.sections
-                ?.mapNotNull(MetaformSection::fields)
-                ?.flatMap { fields -> fields.toList() }
-                ?.filter { field -> field.type == MetaformFieldType.TABLE }
+            val sections = jacksonObjectMapper().nodeFactory.arrayNode()
+            metaform.get("sections").forEach { section ->
+                val objectMapper = jacksonObjectMapper()
+                objectMapper.registerModule(JavaTimeModule())
+                val fields = jacksonObjectMapper().nodeFactory.arrayNode()
+                section["fields"].forEach { field ->
+                    val newField = (field as ObjectNode)
+                    if (newField["type"].asText() == "table") {
 
-            if (!tableFields.isNullOrEmpty()) {
-                throw CustomChangeException("Table field migration is not supported")
+                        val table = objectMapper.createObjectNode()
+
+                        table.put("addRows", newField["addRows"])
+                        table.put("columns", newField["columns"])
+                        newField.put("table", table)
+                    }
+
+                    newField.remove("class")
+                    newField.remove("printable")
+                    newField.remove("source-url")
+                    newField.remove("upload-url")
+                    newField.remove("single-file")
+                    newField.remove("only-images")
+                    newField.remove("max-file-size")
+                    newField.remove("draggable")
+                    newField.remove("src")
+                    newField.remove("addRows")
+                    newField.remove("columns")
+
+                    fields.add(newField)
+                }
+
+
+                val newSection = (section as ObjectNode)
+                newSection.put("fields", fields)
+                sections.add(newSection)
             }
+
+
+            val newMetaform = (metaform as ObjectNode)
+            newMetaform.put("sections", sections)
 
             updateMetaform(
                 connection = connection,
-                metaform = metaform,
+                metaform = newMetaform,
                 metaformId = metaformId
             )
         }
