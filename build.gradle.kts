d8ef2fbb2cd54afe2711466f3ec5cfd0d806d114
JuusoMRusanen <93041083+JuusoMRusanen@users.noreply.github.com>
2026-07-23 13:49:40 +0300

    Add pagination test for completed replies and new permission context JSON

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,6 +99,7 @@ sourceSets["test"].java {
 
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+    systemProperty("api.version", "1.40")
 }
 allOpen {
     annotation("jakarta.ws.rs.Path")
